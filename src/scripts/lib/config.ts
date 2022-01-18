2b8f5700345f8abe561cf2d14c8ad885222eae97
Yong Lin Wang <wangyonglin1999@gmail.com>
2022-01-18 16:44:59 -0500

    chore: bump mpv version on windows

--- src/scripts/lib/config.ts
+++ src/scripts/lib/config.ts
@@ -1,12 +1,12 @@
 export const mpvInstallationInfo = {
   win: {
     x64: {
-      url: 'https://downloads.sourceforge.net/project/mpv-player-windows/stable/mpv-0.33.0-x86_64.7z',
-      hash: 'sha1:6572613643de23e2f999ed8434cb2f0fb6bee909',
+      url: 'https://downloads.sourceforge.net/project/mpv-player-windows/release/mpv-0.34.0-x86_64.7z',
+      hash: 'sha1:4491b548ce87af2626d5cd64548f52a688e24eb8'
     },
     x86: {
-      url: 'https://downloads.sourceforge.net/project/mpv-player-windows/stable/mpv-0.33.0-i686.7z',
-      hash: 'sha1:2428ca258211d71609bcbee70ef2f399a03c06cc',
-    },
-  },
+      url: 'https://downloads.sourceforge.net/project/mpv-player-windows/release/mpv-0.34.0-i686.7z',
+      hash: 'sha1:9837355f3cafa4a509ff4ad4c55670ffd4b7b210'
+    }
+  }
 };
